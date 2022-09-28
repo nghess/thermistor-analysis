bbbd5ba8967c1b81f4b2587f0fd57f6f62366bab
Nate Gonzales Hess <nhess@uoregon.edu>
2022-09-28 01:11:47 -0700

    thermistor lsr-rt

--- thermistor-detect-wm.py
+++ thermistor-detect-wm.py
@@ -10,8 +10,8 @@ data = np.subtract(data, np.min(data)) / (np.max(data)-np.min(data))
 time = [t for t in range(len(data))]
 
 # Display window dims
-width = 3000
-height = 600
+width = 500
+height = 500
 mid_w = int(width/2)
 mid_h = int(height/2)
 end = width-1
@@ -47,6 +47,9 @@ def get_slope(traj, step, scale, img):
     return p1, vec
 
 
+change_tally = []
+breath = (0, 0, 255)
+
 while True:
     i += 1  # Time
     therm = int((1-data[i])*height)-1  # Thermistor value
@@ -54,12 +57,12 @@ while True:
     #Clear frame
     display = np.zeros((height, width, 3), dtype=np.uint8)
     # Latest raw thermistor value
-    display[therm, end] = (0, 0, 255)
+    display[therm, end-margin] = (0, 0, 255)
 
     # Compile trail for raw values
     trail_raw.append(therm)
-    if len(trail_raw) == end:
-        trail_raw = trail_raw[1:]
+    #if len(trail_raw) == end-margin:
+    #    trail_raw = trail_raw[1:]
     for t in range(1, len(trail_raw)):  # For loops are way too slow. Figure out alternative.
         accel = (max_delta*height) / abs(trail_raw[-t]-trail_raw[-t-1])
         #print(abs(trail_raw[-t]-trail_raw[-t-1]))
@@ -69,9 +72,23 @@ while True:
     if len(trail_raw) >= k:
         running_avg = int(np.mean(trail_raw[-k:]))
         trail_avg.append(running_avg)
+    #    if len(trail_avg) == end-margin:
+    #        trail_avg = trail_avg[1:]
         # Draw running avg trail
+        if len(trail_avg) >= 3:
+            # Get differential
+            cur_diff = np.ediff1d(trail_avg[-2:])[0]
+            change_tally.append(cur_diff)
+        if len(change_tally) > 3:
+            change_tally = change_tally[1:]
+            #print(change_tally)
+
         for t in range(1, len(trail_avg)):
-            display[trail_avg[-t], end-t-margin] = (255, 255, 0)
+            if sum(change_tally) > 10:
+                breath = (255, 0, 0)
+            else:
+                breath = (0, 0, 255)
+            display[trail_avg[-t], end-t-margin] = breath
 
         if len(trail_avg) > step:
             # Show slope
@@ -83,19 +100,16 @@ while True:
         cur_y = round(1-(running_avg/height), 2)
         cv2.putText(display, str(cur_y), (width-margin+50, trail_avg[-1]), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 0))
 
-    # Get differential
-    #cur_diff = np.ediff1d(trail_avg[-2:])
-    #print(cur_diff)
-
     # Print timestamp
     cv2.putText(display, f'{i} ms', (5, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
 
     # Clear out of frame values from trail
     if len(trail_raw) == end-margin:
-        trail_raw = trail_raw[1:]
+       trail_raw = trail_raw[1:]
 
     if len(trail_avg) == end-margin:
         trail_avg = trail_avg[1:]
+
 
     cv2.imshow('Thermistor Signal', display)
     cv2.waitKey(16)
--- thermistor-lsr-window.py
+++ thermistor-lsr-window.py
@@ -10,18 +10,13 @@ data = np.subtract(data, np.min(data)) / (np.max(data)-np.min(data))
 time = [t for t in range(len(data))]
 
 # Display window dims
-width = 800
+width = 300
 height = 300
 middle = int(width/2)
 end = width-1
 
-
 i = 0  # timestep
-k = 20  # K for rolling mean
 trail_raw = []
-trail_avg = []
-running_avg = 0  # var to report avg Y value
-
 
 while True:
     i += 1  # Time
@@ -39,28 +34,10 @@ while True:
     for t in range(1, len(trail_raw)):  # For loops are way too slow. Figure out alternative.
         display[trail_raw[-t], end-t] = (0, 0, 128)
 
-    # Compile trail for AVG values
-    if len(trail_raw) >= k:
-        running_avg = int(np.mean(trail_raw[-k:]))
-        trail_avg.append(running_avg)
-        # Draw running avg trail
-        for t in range(1, len(trail_avg)):
-            display[trail_avg[-t], end-t] = (255, 255, 0)
-
     # Print timestamp
     cv2.putText(display, f'{i} ms', (5, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
-    # Print avg Y Value
-    cur_y = round(1-(running_avg/height), 2)
-    cv2.putText(display, str(cur_y), (width-50, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 0))
 
-
-
-    # Clear out of frame values from trail
-    if len(trail_raw) == end:
-        trail_raw = trail_raw[1:]
-
-    if len(trail_avg) == end:
-        trail_avg = trail_avg[1:]
+    # Print Slope Value (once m is calculated, report slope here)
 
 
 
