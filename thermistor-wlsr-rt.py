227d883805619994af1f0226602fbcc9247cc409
nghess <nhess@uoregon.edu>
2022-09-29 11:02:57 -0700

    LSR realtime

--- thermistor-wlsr-rt.py
+++ thermistor-wlsr-rt.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 # Import .dat and zip raw and smoothed into DF
-data = np.fromfile('.dat/thermistor2.dat', dtype=float)
+data = np.fromfile('.dat/thermistor1.dat', dtype=float)
 # Normalize
 data = np.subtract(data, np.min(data)) / (np.max(data)-np.min(data))
 # Time axis
@@ -14,24 +14,25 @@ height = 500
 middle = int(width/2)
 end = width-1
 
-#LSR Window
+# LSR Window
 window = 20
+db = 100  # debounce
 
-i = 0  # timestep
-debounce = 0  # debounce timer
+i = 0  # initiate timestep
+debounce = 0  # initiate debounce timer
 trail_raw = []  # list to store data as it comes in
 slope_compare = []  # compare current and previous slope to detect change
-sniffs = [] # to store sniffs as they are detected
+sniffs = []  # to store sniffs as they are detected
 
 
 while True:
     i += 1  # Time
-    debounce -= 1  # debounce timer for sniffs
+    debounce -= 1  # Debounce timer for sniffs
 
-    therm = int((1-data[i])*height)-1  # Thermistor value
+    therm = int((1-data[i])*height)-1  # Current thermistor value
 
     #Clear frame
-    display = np.zeros((height, width, 3), dtype=np.uint8)
+    display = np.ones((height, width, 3), dtype=np.uint8)*64
     # Latest raw thermistor value
     display[therm, end] = (0, 0, 255)
 
@@ -39,8 +40,10 @@ while True:
     trail_raw.append(therm)
     if len(trail_raw) == end:
         trail_raw = trail_raw[1:]
-    for t in range(1, len(trail_raw)):  # For loops are way too slow. Figure out alternative.
-        display[trail_raw[-t], end-t] = (0, 0, 128)
+
+    # Draw data
+    for t in range(1, len(trail_raw)):  # For loops are slow. Figure out indexing alternative.
+        display[trail_raw[-t], end-t] = (255, 255, 255)
 
     # Print timestamp
     cv2.putText(display, f'{i} ms', (5, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
@@ -66,25 +69,26 @@ while True:
             color = (0, 0, 255)
 
         # Try to pull out sniffs
-        if len(slope_compare) == 2 and slope_compare[1] > 1 > slope_compare[0] and debounce <= 0:
+        if len(slope_compare) == 2 and slope_compare[1] > 1.1 and debounce <= 0:
             sniffs.append([width, int(y2)])
-            debounce = 75  # initiate debounce after detection
+            debounce = db  # Reset debounce after detection
 
+        # Draw circle at sniff detection
         for s in range(len(sniffs)):
-            cv2.circle(display, sniffs[s], 2, (255, 255, 255), -1, lineType=cv2.LINE_AA)
+            cv2.circle(display, sniffs[s], 3, (255, 255, 0), -1, lineType=cv2.LINE_AA)
 
-        for x in sniffs:
-            x[0] -= 1
+        # Decrement sniff marker x position
+        for s in sniffs:
+            s[0] -= 1
 
-        # LSR Line
+        # Draw LSR Line
         cv2.line(display, (width-window, int(y1)), (width, int(y2)), color, 1)
 
-    # Print Slope Value (once m is calculated, report slope here)
-
-
+    # Display Results
     cv2.imshow('Thermistor Signal', display)
-    cv2.imwrite("output/lsr-rt/" + str(i) + ".png", display)
+    #cv2.imwrite("output/lsr-rt/" + str(i) + ".png", display)
     cv2.waitKey(8)
 
+    # If all data has been read, restart feed
     if i == len(data)-1:
         i = 0
